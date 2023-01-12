066982428fdcf74507e94aee0a49a9783f722e76
Lina <coldwv@gmail.com>
2023-01-12 17:46:55 +0100

    added mobile version

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 function displayTempF(response) {
   document.querySelector(`#today-temp`).innerHTML = `${Math.round(response.data.main.temp)}&degF`;
+  document.querySelector(`#today-temp2`).innerHTML = `${Math.round(response.data.main.temp)}&degF`;
 }
 
 function displayTempC(response) {
   document.querySelector(`#today-temp`).innerHTML = `${Math.round(response.data.main.temp)}&degC`;
+  document.querySelector(`#today-temp2`).innerHTML = `${Math.round(response.data.main.temp)}&degC`;
 }
 
 function getTempF(event) {
@@ -29,6 +31,7 @@ function displayDate() {
   let minutes = date.getMinutes();
   if (minutes < 10) minutes = `0${minutes}`;
   document.querySelector(`#date`).innerHTML = `${day} ${hours}:${minutes}`;
+  document.querySelector(`#date2`).innerHTML = `${day} ${hours}:${minutes}`;
   setTimeout(displayDate, 1000);
 }
 
@@ -65,10 +68,11 @@ function generateForecast(response) {
       else if (forecastDay.weather[0].main === `Thunderstorm`) 
         addingElement = `fa-bolt`;
       forecastRow += `<div class="col-4" > <div class="p-3 text-center forecast-cell">
-      <h5 id="one">${getForecastDay(forecastDay.dt)}</h5> <i class="fas ${addingElement} icon " id="forecastIcon"></i>
+      <h5 class="cell-day">${getForecastDay(forecastDay.dt)}</h5> <i class="fas ${addingElement} icon " id="forecastIcon"></i>
       <p id="max"><strong>${Math.round(forecastDay.temp.max)}</strong>&deg ${Math.round(forecastDay.temp.min)}&deg</p></div></div>`;
     }});
   document.querySelector(`#forecastCells`).innerHTML = forecastRow + `</div>`;
+  document.querySelector(`#forecastCells2`).innerHTML = forecastRow + `</div>`;
 }
 
 //is called when #user-input #search-input and #user-location are clicked
@@ -81,6 +85,11 @@ function showSearchedCity(apiResponse) {
   document.querySelector(`#description-text`).innerHTML = apiResponse.data.weather[0].main;
   document.querySelector(`#humidity`).innerHTML = `${apiResponse.data.main.humidity}%`;
   document.querySelector(`#wind`).innerHTML = `${Math.round((apiResponse.data.wind.speed * 18) / 5)} km/h`;
+  document.querySelector(`#displayed-city-name2`).innerHTML = apiResponse.data.name;  
+  document.querySelector(`#today-temp2`).innerHTML = `${Math.round(apiResponse.data.main.temp)}&degC`;
+  document.querySelector(`#description-text2`).innerHTML = apiResponse.data.weather[0].main;
+  document.querySelector(`#humidity2`).innerHTML = `${apiResponse.data.main.humidity}%`;
+  document.querySelector(`#wind2`).innerHTML = `${Math.round((apiResponse.data.wind.speed * 18) / 5)} km/h`;
 
   //forecast data
  let apiKey= 'c119ffef35b7245a5e03b6e5724ae961';
@@ -107,6 +116,27 @@ function showSearchedCity(apiResponse) {
     document.querySelector(`#weather-icon`).setAttribute(`class`, `fas fa-snowflake weather-icon`);
   else if (document.querySelector(`#description-text`).textContent === `Fog`) 
     document.querySelector(`#weather-icon`).setAttribute(`class`, `fas fa-smog weather-icon`);
+    
+    if (document.querySelector(`#description-text2`).textContent === `Clear`) 
+    document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-sun weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Rain`) 
+    document.querySelector(`#weather-icon2`).setAttribute(`class`,`fas fa-cloud-showers-heavy weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Clouds`)  
+    document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-cloud weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Thunderstorm`) 
+   document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-bolt weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Haze`) 
+   document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-smog weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Mist`) 
+    document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-smog weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Drizzle`) 
+    document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-cloud-rain weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Smoke`)
+    document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-smog weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Snow`) 
+    document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-snowflake weather-icon`);
+  else if (document.querySelector(`#description-text2`).textContent === `Fog`) 
+    document.querySelector(`#weather-icon2`).setAttribute(`class`, `fas fa-smog weather-icon`);
   }
 }
 
@@ -119,6 +149,7 @@ function searchCity(city) {
 function display_city(event) {
   event.preventDefault();
   searchCity(document.querySelector("#enter-city").value);
+  searchCity(document.querySelector("#enter-city2").value);
 }
 
 //is called when #user-location is clicked
@@ -133,10 +164,15 @@ function getCurrentLocation() {
 }
 
 document.querySelector(`#user-input`).addEventListener(`submit`, display_city);
+document.querySelector(`#user-input2`).addEventListener(`submit`, display_city);
 document.querySelector(`#search-input`).addEventListener(`click`, display_city);
+document.querySelector(`#search-input2`).addEventListener(`click`, display_city);
 document.querySelector(`#user-location`).addEventListener(`click`, getCurrentLocation);
+document.querySelector(`#user-location2`).addEventListener(`click`, getCurrentLocation);
 document.querySelector(`#fahrenheit`).addEventListener(`click`, getTempF);
+document.querySelector(`#fahrenheit2`).addEventListener(`click`, getTempF);
 document.querySelector(`#celcius`).addEventListener(`click`, getTempC); 
+document.querySelector(`#celcius2`).addEventListener(`click`, getTempC); 
 
 var myVar;
 
@@ -148,5 +184,6 @@ function showPage() {
   displayDate();
   searchCity("Moscow");  
   document.getElementById("regular-website").style.display = "block";
+  document.getElementById("regular-website2").style.display = "block";
   document.body.classList.add('loaded');
 }
